685c79541734698fe1b4f634ecf75a57abdb9958
fbs2015 <f.b.s2009@hotmail.com>
2021-07-21 22:24:56 -0300

    Teste Operações sobre arrays criado.

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.collection
+
+fun main(){
+
+    val salarios = doubleArrayOf(1250.0, 2000.0, 2500.0, 3500.0, 5000.0, 1350.0, 2450.0)
+    salarios.sort()
+    salarios.forEach{println(it)}
+
+    println("---------------------------")
+
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor slario: ${salarios.minOrNull()}")
+    println("Media salarial: ${salarios.average()}")
+
+    println("------------------------------")
+    val salariosMaiores2500 = salarios.filter{it > 2500.0}
+    salariosMaiores2500.forEach { println(it) }
+
+}
